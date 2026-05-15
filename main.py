c7ff285a2030717ac7d5117d90cdba942f809705
HaeengIn <haeengin@gmail.com>
2026-05-15 01:23:15 +0000

    Initialize web app

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from template_config import templates
+
+app = FastAPI()
+app.mount("static", StaticFiles(directory="/static"), name="static")
+
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
